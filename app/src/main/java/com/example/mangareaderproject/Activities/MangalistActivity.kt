5d66bcbe2c07494a503d6d365a1d4f0b00308ea0
JasonHaque <zahinjason220434@gmail.com>
2020-01-21 14:22:06 +0600

    added new activity

---
+++ app/src/main/java/com/example/mangareaderproject/Activities/MangalistActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mangareaderproject.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mangareaderproject.R
+
+class MangalistActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mangalist)
+    }
+}
