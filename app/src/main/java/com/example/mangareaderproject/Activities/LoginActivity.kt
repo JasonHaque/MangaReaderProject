ac6470105d717c4d1dace7537499b24bb264ee08
JasonHaque <zahinjason220434@gmail.com>
2020-01-23 10:16:30 +0600

    added input validation

--- app/src/main/java/com/example/mangareaderproject/Activities/LoginActivity.kt
+++ app/src/main/java/com/example/mangareaderproject/Activities/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.mangareaderproject.Activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.mangareaderproject.R
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -19,5 +20,25 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this,SignUpActivity::class.java)
             startActivity(intent)
         }
+
+        log_in_button.setOnClickListener {
+
+            val email = log_in_mail.text.toString()
+            val password = log_in_password.text.toString()
+            val error  = validateInputs(email, password)
+
+            if (!error.equals("")){
+                Toast.makeText(this,error,Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
+    }
+
+    fun validateInputs( email:String, password:String):String{
+        if (email.isEmpty() || password.isEmpty()){
+            return "Fill in the Fields properly"
+        }
+
+        return ""
     }
 }
